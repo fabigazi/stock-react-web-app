989526e1f494043b0a3553f4421ebad8921b2570
fabigazi <fgaziano98@gmail.com>
2023-06-22 15:52:51 -0400

    connections to the node server

--- src/tuiter/tuits-service.js
+++ src/tuiter/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER = "https://tuiter-node-server-app-su1-23.onrender.com";
+const SERVER = "https://stock-node-server-app.onrender.com";
 const BASE_API = `${SERVER}/api`;
 
 const request = axios.create({
